2f55e9158d393a86fd49a6933c26d77d1f9264c7
Tudor Comaniciu <tmcomaniciu@gmail.com>
2023-11-22 18:51:39 -0500

    cleaned up CRA initial files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+// import Gallery from "./components/Gallery";
+// import SearchBar from "./components/SearchBar";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // let [search, setSearch] = useState("");
+  // let [message, setMessage] = useState("Search for Music!");
+  // let [data, setData] = useState([]);
+
+  return <div>Hello</div>;
 }
 
 export default App;
